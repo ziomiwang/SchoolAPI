b257c74d961a83f3160d217bc70efd867ad6e08d
ziomiwang <ziomiwang@gmail.com>
2024-02-20 00:24:33 +0100

    init

---
+++ src/test/java/org/example/schoolapi/SchoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schoolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
